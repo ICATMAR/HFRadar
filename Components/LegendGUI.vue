ae542d14f31ed931fa9a9762322fb22cb99480c1
gerardllorach <gerard.llorach@gmail.com>
2023-02-16 17:54:47 +0100

    added tooltip to legend

--- Components/LegendGUI.vue
+++ Components/LegendGUI.vue
@@ -3,6 +3,13 @@
   <div id='legendGUI' ref='legendGUI' @mouseover="mouseOver" @mouseleave="mouseLeave">
 
     <div v-show="legendsLoaded">
+      <!-- Tooltip -->
+      <div v-show="!isMouseOver && currentValue !=''">
+        <div class="tooltipLegend" ref="tooltipLegend">{{currentValue}} cm/s</div>
+        <div class="tooltipLegendBar" ref="tooltipLegendBar">|</div>
+      </div>
+
+      <!-- Legend -->
       <img class="selLegend" :src="legendSrc">
       <div class="rangeValuesBox">
         <div class="leftRange">-100 cm/s</div>
@@ -39,6 +46,17 @@ export default {
       this.legendsLoaded = true;
       this.legendSrc = legends[this.legendIndex].img.src;
       this.emitLegendChanged(this.legends[this.legendIndex]);
+    });
+    // When mouse clicks a data point
+    window.eventBus.on('ClickedDataPoint', dataPoint => {
+
+      this.currentValue = dataPoint['Velocity (cm/s)'].toFixed(1);
+      this.$refs.tooltipLegend.style.left = (100 * (this.currentValue - this.HFRADARRANGE[0]) / (this.HFRADARRANGE[1] - this.HFRADARRANGE[0])) + '%';
+      this.$refs.tooltipLegendBar.style.left = (100 * (this.currentValue - this.HFRADARRANGE[0]) / (this.HFRADARRANGE[1] - this.HFRADARRANGE[0])) + '%';
+    })
+    // When map deselects a data point
+    window.eventBus.on('DeselectedDataPoint', () => {
+      this.currentValue = '';
     })
   },
   data (){
@@ -47,6 +65,9 @@ export default {
       legendIndex: 0,
       legendSrc: '',
       isMouseOver: false,
+      // Tooltip
+      HFRADARRANGE: [-100, 100],
+      currentValue: '',
     }
   },
   methods: {
@@ -99,6 +120,23 @@ img {
   border-radius: 5px;
 }
 
+
+.tooltipLegend {
+  position:absolute;
+  top: -20px;
+  color:white;
+  transform: translateX(-50%);
+  -ms-transform: translateX(-50%);
+}
+.tooltipLegendBar {
+  position:absolute;
+  top: 5px;
+  font-size:large;
+  text-shadow: 0px 0px 5px rgb(0, 0, 0);
+  color:white;
+  transform: translateX(-50%);
+  -ms-transform: translateX(-50%);
+}
 .selLegend {
   margin-top: 10px;
   margin-bottom: 10px;
