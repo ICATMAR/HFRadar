e26904d1d60ad805d7c15eeb4ac6d725670e7aa0
Gerard Llorach <gerard.llorach@gmail.com>
2023-08-11 11:00:53 +0200

    calendar better interface, now close and emit events missing

--- Components/BottomSection/Calendar.vue
+++ Components/BottomSection/Calendar.vue
@@ -1,39 +1,61 @@
 <template>
   <div class="calendarContainer">
-    <!-- Year -->
-    <div class="yearContainer">
+    <!-- Info -->
+    <div class="white-text">Select the date (UTC-0)</div>
+    <!-- YEAR -->
+    <!-- Calendar -->
+    <div class="yearContainer" v-show="timeScaleToShow == 'year'">
       <button v-for="yy in years" class="clickable white-text"
         :class="[yy.isSelected ? 'yearButton button-active' : yy.num % 2 == 0 ? 'yearButton' : 'yearButton even']"
         @click="onYearClicked($event)" :key="yy.num" :id="yy.num" :title="yy.num"
         >{{yy.name}}</button>
     </div>
-    <!-- Month -->
-    <div class="timeline" ref="monthTimeline">
+    <!-- MONTH -->
+    <!-- Selected date -->
+    <div class="text-white selectedDateContainer" v-show="timeScaleToShow == 'month'">
+      <div class="clickable" @click="changeYearClicked(-1)">&lt;</div>
+      <div class="clickable" @click="timeScaleToShow = 'year'">{{ tempTmst.substring(0,4) }}</div>
+      <div class="clickable" @click="changeYearClicked(1)">></div>
+    </div>
+    <!-- Calendar -->
+    <div class="timeline" ref="monthTimeline" v-show="timeScaleToShow == 'month'">
       <button v-for="mm in months" class="monthButton clickable white-text"
         :class="[mm.isSelected ? 'button-active' : '']"
         @click="onMonthClicked($event)" :key="mm.key" :id="mm.key" :title="$i18n.t(mm.title) + ', ' + mm.year"
         >{{$t(mm.title)}}</button>
     </div>
-    <!-- Day -->
-    <div class="timeline daysContainer" ref="dayTimeline" v-show="days.length!=0">
+    <!-- DAY -->
+      <!-- Selected date -->
+    <div class="text-white selectedDateContainer" v-show="timeScaleToShow == 'day'">
+      <div class="clickable" @click="changeMonthClicked(-1)">&lt;</div>
+      <div class="clickable" @click="timeScaleToShow = 'month'">{{monthAbbr[parseInt(tempTmst.substring(5,7))-1][2]}} , {{ tempTmst.substring(0,4) }}</div>
+      <div class="clickable" @click="changeMonthClicked(1)">></div>
+    </div>
+      <!-- Calendar -->
+    <div class="timeline daysContainer" ref="dayTimeline" v-show="timeScaleToShow == 'day'" >
       <button v-for="dd in days" class="clickable white-text dayButton" :class="[dd.hasData == false ? 'noData' : dd.isSelected ? 'button-active' : '']"
         @click="onDayClicked($event)" :key="dd.key" :id="dd.key" :title="dd.title"
         >{{dd.name}}</button>
     </div>
-    <!-- Hour -->
-    <div class="timeline" ref="dayTimeline" v-show="hours.length!=0">
+    <!-- HOUR -->
+      <!-- Selected date -->
+    <div class="text-white selectedDateContainer" v-show="timeScaleToShow == 'hour'">
+      <div class="clickable" @click="changeDayClicked(-1)">&lt;</div>
+      <div class="clickable" @click="timeScaleToShow = 'day'">{{ parseInt(tempTmst.substring(8,10)) }} , {{monthAbbr[parseInt(tempTmst.substring(5,7))-1][2]}} , {{ tempTmst.substring(0,4) }}</div>
+      <div class="clickable" @click="changeDayClicked(1)">></div>
+    </div>
+      <!-- Calendar -->
+    <div class="timeline" ref="dayTimeline" v-show="timeScaleToShow == 'hour'">
       <button v-for="hh in hours" class="clickable white-text hourButton" :class="[hh.hasData == false ? 'noData' : hh.isSelected ? 'button-active' : '']"
         @click="onHourClicked($event)" :key="hh.key" :id="hh.key" :title="hh.title"
         >{{hh.name}}:00</button>
     </div>
 
-    <!-- Info local time -->
-    <div class="white-text">Date is in UTC+0</div>
 
     <!-- Cancel / Ok -->
     <div class="acceptContainer">
-      <button class="closeButton">Cancel</button>
-      <button class="acceptButton">OK</button>
+      <button class="closeButton" @click="cancelClicked">Cancel</button>
+      <button class="acceptButton" @click="acceptClicked">OK</button>
     </div>
   </div>
 </template>
@@ -46,6 +68,13 @@ export default {
     // Start and end dates if startDate and endDate are flexible
     this.startDate = new Date(2023, 3, 1); 
     this.endDate = new Date();
+
+    // Start/End year and month
+    this.startYear = this.startDate.getUTCFullYear();
+    this.startMonth = this.startDate.getUTCMonth();
+    this.startHour = this.startDate.getUTCHours();
+    this.endYear = this.endDate.getUTCFullYear();
+    this.endMonth = this.endDate.getUTCMonth();
 
     // Month names
     this.monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -64,12 +93,19 @@ export default {
       ['D', 'Dec', 'December']
     ];
 
+    // Start end dates
+
+
   },
   mounted() {
+
+    this.createHTML(this.tempTmst);
      
     // EVENTS
     window.eventBus.on("HFRadarDataLoaded", tmst => {
-        this.createHTML();
+      console.log(tmst);
+      //let tempTmst = window.GUIManager.currentTmst;
+      //this.createHTML(tempTmst);
     });
     
   },
@@ -79,20 +115,102 @@ export default {
       years: [],
       days: [],
       hours: [],
+      timeScaleToShow: 'year',
+      tempTmst: '2023-08-11T06:00:00.000Z',
     }
   },
   methods: {
-    createHTML: function() {
-      let startYear = this.startDate.getUTCFullYear();
-      let startMonth = this.startDate.getUTCMonth();
-      let startHour = this.startDate.getUTCHours();
-      let endYear = this.endDate.getUTCFullYear();
-      let endMonth = this.endDate.getUTCMonth();
-      let endHour = this.endDate.getUTCHours();
+    // USER INTERACTION
+    onYearClicked: function(e){
+      let year = e.target.id;
+      // Modify tempTmst
+      this.tempTmst = year + this.tempTmst.substring(4);
+      // Update HTML (TODO: not optimal)
+      this.createHTML(this.tempTmst);
+      this.timeScaleToShow = 'month';
+    },
+    onMonthClicked: function(e){
+      // Get selected month
+      let month = parseInt(e.target.id.split('-')[0]);
+      
+      // Modify tempTmst
+      this.tempTmst = this.tempTmst.substring(0,5) + String((month+1)).padStart(2, '0') + this.tempTmst.substring(7);
+      // Update HTML
+      this.createHTML(this.tempTmst);
+      this.timeScaleToShow = 'day';
+    },
+    onDayClicked: function(e){
+      let day = parseInt(e.target.id.split('-')[0]);
+      this.tempTmst = this.tempTmst.substring(0,8) + String(day).padStart(2, '0') + this.tempTmst.substring(10);
+      this.createHTML(this.tempTmst);
+      this.timeScaleToShow = 'hour';
+    },
+    onHourClicked: function(e){
+      let hour = parseInt(e.target.id.split('-')[0]);
+      this.tempTmst = this.tempTmst.substring(0,11) + String(hour).padStart(2, '0') + this.tempTmst.substring(13);
+      this.timeScaleToShow = 'year';
+      // TODO: CLOSE AND UPDATE TIMESTAMP
+    },
+
+
+    changeYearClicked: function(incr){
+      let currentDate = new Date(this.tempTmst);
+      currentDate.setUTCFullYear(currentDate.getUTCFullYear() + incr);
+      let cYear = currentDate.getUTCFullYear();
+      // Find limits
+      if (cYear >= this.startYear && cYear <= this.endYear){
+        this.tempTmst = currentDate.toISOString();
+        this.createHTML(this.tempTmst);
+      }
+    },
+    changeMonthClicked: function(incr){
+      let currentDate = new Date(this.tempTmst);
+      currentDate.setUTCMonth(currentDate.getUTCMonth() + incr);
+      let cYear = currentDate.getUTCFullYear();
+      let cMonth = currentDate.getUTCMonth();
+      // Find limits
+      let isBeforeStartDate = (cYear == this.startYear && cMonth < this.startMonth || cYear < this.startYear);
+      let isAfterEndDate = (cYear == this.endYear && cMonth > this.endMonth || cYear > this.endYear);
+      // If inside limits, update
+      if (!isBeforeStartDate && !isAfterEndDate){
+        this.tempTmst = currentDate.toISOString();
+        this.createHTML(this.tempTmst);
+      }
+    },
+    changeDayClicked: function(incr){
+      let currentDate = new Date(this.tempTmst);
+      currentDate.setUTCDate(currentDate.getUTCDate() + incr);
+      // Set hours to 0, so the end date is not surpassed when comparing dates (only year, month and day should be taken into account)
+      // TODO: It affects negatively the start date (hour should be set to 23:59), but it is not as relevant as the last dataset
+      currentDate.setUTCHours(0);
+      // Find limits
+      if (currentDate > this.startDate && currentDate < this.endDate){
+        this.tempTmst = currentDate.toISOString();
+        this.createHTML(this.tempTmst);
+      }
+    },
+
+    acceptClicked: function(){
+      // Send event
+      
+      // Hide the window
+    },
+    cancelClicked: function(){
+
+    },
+
+
+
+
+    // INTERNAL METHODS
+    createHTML: function(tmst) {
+      let startYear = this.startYear;
+      let startMonth = this.startMonth;
+      let endYear = this.endYear;
+      let endMonth = this.endMonth;
 
       // Selected date
-      console.log(window.GUIManager.currentTmst);
-      let selDate = new Date(window.GUIManager.currentTmst);
+      let selDate = new Date(tmst);
       let selYear = selDate.getUTCFullYear();
       let selMonth = selDate.getUTCMonth();
       let selDay = selDate.getUTCDate();
@@ -210,8 +328,10 @@ export default {
   bottom: 0;
 
   width: clamp(200px, 50%, 500px);
+  height: fit-content;
   padding: 30px;
 
+  top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 
@@ -223,6 +343,8 @@ export default {
   flex-wrap: nowrap;
   justify-content: center;
   align-items: center;
+  
+  z-index: 10;
 }
 
 .timeline {
@@ -277,5 +399,18 @@ export default {
   background: grey;
   user-select: none;
   cursor: default;
+}
+
+.selectedDateContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 80%;
+}
+
+.selectedDateContainer > div {
+  padding: 10px;
 }
 </style>
