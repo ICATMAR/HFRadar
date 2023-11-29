9618339bfcd740b0accbdc46b8a26fb95d5a941f
Gerard Llorach <gerard.llorach@gmail.com>
2023-11-29 11:49:53 +0100

    improved the overlay panel for the HFRadar

--- Components/Map/OverlayWaveData.vue
+++ Components/Map/OverlayWaveData.vue
@@ -5,27 +5,41 @@
     <div v-for="key in Object.keys(radarData)" :id="key" :ref="key" class="radarPanelContainer">
 
       <!-- Wave panel -->
-      <div class="wavepanel" :class="[isTooFar ? 'hide' : 'show']" v-if="radarData[key].hasData">
+      <div class="wavepanel" :class="[isTooFar ? 'hide' : 'show']">
         <!-- Site -->
         <div class="radarTitle">
           <span><strong>{{ radarData[key].site }}</strong></span>
           <a href="https://www.icatmar.cat" target="_blank" rel="noopener noreferrer" class="icon-str">i</a>
         </div>
-        <!-- Wave data -->
-        <div v-show="radarData[key].waveHeight != undefined">
-          <span><strong>Waves: </strong>{{ radarData[key].waveHeight }} m, {{ radarData[key].wavePeriod }} s, {{ radarData[key].waveBearing }}</span>
-          <span class="fa" :style="{transform: 'rotate('+ (radarData[key].waveBearingValue-45+180) +'deg)' }">&#xf124;</span>
+        <!-- Data -->
+        <div v-if="radarData[key].hasData">
+          <!-- Wave data -->
+          <div v-show="radarData[key].waveHeight != undefined">
+            <span><strong>Waves: </strong>{{ radarData[key].waveHeight }} m, {{ radarData[key].wavePeriod }} s, {{ radarData[key].waveBearing }}</span>
+            <span class="fa" :style="{transform: 'rotate('+ (radarData[key].waveBearingValue-45+180) +'deg)' }">&#xf124;</span>
+          </div>
+          <!-- Wind data -->
+          <div v-show="radarData[key].windBearing != undefined">
+            <span><strong>Wind direction:</strong> {{ radarData[key].windBearing }}</span>
+            <span class="fa" :style="{transform: 'rotate('+ (radarData[key].windBearingValue-45+180) +'deg)' }">&#xf124;</span>
+          </div>
         </div>
-        <!-- Wind data -->
-        <div v-show="radarData[key].windBearing != undefined">
-          <span><strong>Wind direction:</strong> {{ radarData[key].windBearing }}</span>
-          <span class="fa" :style="{transform: 'rotate('+ (radarData[key].windBearingValue-45+180) +'deg)' }">&#xf124;</span>
-        </div>
+
+        <!-- No wind-wave data, but file exists-->
+        <div v-else-if="radarData[key].fileStatus == 2"><span>No wave or wind data</span></div>
+        
+        
+        <!-- Loading -->
+        <div v-if="radarData[key].fileStatus == 3"><span>Loading...</span></div>
+        <!-- No file -->
+        <div v-else-if="radarData[key].fileStatus == 0"><span>No file</span></div>
+
       </div>
+      
 
       
       <!-- Radar icon -->
-      <img class="icon-str icon-big icon-img radarIcon" src="/HFRadar/Assets/Images/radar.svg">
+      <img class="icon-str icon-big icon-img radarIcon" :class="[radarData[key].fileStatus == 0 ? 'iconNoFile' : radarData[key].fileStatus == 3 ? 'iconLoading' : '']" src="/HFRadar/Assets/Images/radar.svg">
 
     </div>
   </div>
@@ -44,8 +58,15 @@ export default {
     // EVENTS
     // HFRadarLoaded
     window.eventBus.on('HFRadarDataLoaded', tmst => {
+      // Create radar data object
+      if (Object.keys(this.radarData).length == 0){
+        this.createRadarDataObject();
+      }
+      
       if (tmst)
         this.updateContent(tmst);
+      else
+        this.updateContent(window.GUIManager.currentTmst);
     });
     // Selected date changed (slider moves or drag and drop files)
     window.eventBus.on('DataStreamsBar_SelectedDateChanged', (tmst) =>{
@@ -61,63 +82,107 @@ export default {
   },
   methods: {
     // INTERNAL
+    createRadarDataObject: function(){
+      // Create radar objects and add to map
+      let radars = window.DataManager.HFRadars;
+      Object.keys(radars).forEach(key => {
+        let radar = radars[key];
+        if (radar.constructor.name == "HFRadar"){
+          // Determine file status
+          let tmst = window.GUIManager.currentTmst;
+          let fileStatus = window.DataManager.hourlyDataAvailability[tmst.substring(0,13) + 'Z'][radar.Site];
+          
+          this.radarData[radar.UUID] = {
+            "site": radar.Site, 
+            "hasData": false,
+            "fileStatus": fileStatus,
+          };
+
+          if (this.map == undefined){
+            this.map = this.$parent.map;
+          }
+
+          // Get radar location
+          let location = radar.getRadarOrigin();
+          let coord3857 = ol.proj.fromLonLat([location[0], location[1]]);
+          // Next tick generates the html in v-for thus we can link the element to the map overlay
+          this.$nextTick(() => {
+            // Wave info
+            const waveInfo = new ol.Overlay({
+              position: coord3857,
+              positioning: 'center-right',
+              element: this.$refs[radar.UUID],
+              stopEvent: false,
+            });
+            this.map.addOverlay(waveInfo);
+          })
+
+        }
+      })
+      
+
+      
+    },
+
+
     updateContent: function(tmst){
+
+      Object.keys(this.radarData).forEach(uuid => {
+        this.radarData[uuid].hasData = false;
+      });
+
       // Get radars
-      let radars = window.DataManager.getRadarsDataOn(tmst);
+      let radars = window.DataManager.HFRadars;
       // Iterate radars
-      radars.forEach(radar => {
+      Object.keys(radars).forEach(uuid => {
+        let radar = radars[uuid];
         // Check if radar is HFRadar and if it was wave data
-        if (radar.constructor.name == "HFRadar" && radar.waveHourlyData){
-          // Check if there is data on that date
-          if (radar.waveHourlyData[tmst] || radar.windHourlyData[tmst]){
-            // Create if it does not exist and create map overlay
-            if (this.radarData[radar.UUID] == undefined){
-              this.radarData[radar.UUID] = {"site": radar.Site, "hasData": true};
+        if (radar.constructor.name == "HFRadar"){
+          if (radar.waveHourlyData){
+            // Check if there is data on that date
+            if (radar.waveHourlyData[tmst] || radar.windHourlyData[tmst]){
 
-              if (this.map == undefined){
-                this.map = this.$parent.map;
-              }
-              // Get radar location
-              let location = radar.getRadarOrigin();
-              let coord3857 = ol.proj.fromLonLat([location[0], location[1]]);
-              // Next tick generates the html in v-for thus we can link the element to the map overlay
-              this.$nextTick(() => {
-                // Wave info
-                const waveInfo = new ol.Overlay({
-                  position: coord3857,
-                  positioning: 'center-right',
-                  element: this.$refs[radar.UUID],
-                  stopEvent: false,
-                });
-                this.map.addOverlay(waveInfo);
-              })
-            
-            }
+              // Radar has data
+              this.radarData[radar.UUID].hasData = true;
 
-            // Radar has data
-            this.radarData[radar.UUID].hasData = true;
-
-            // Update wave data
-            if (radar.waveHourlyData[tmst]){
-                this.radarData[radar.UUID].waveHeight = radar.waveHourlyData[tmst].MWHT.toFixed(2);
-                this.radarData[radar.UUID].wavePeriod = radar.waveHourlyData[tmst].MWPD.toFixed(1);
-                this.radarData[radar.UUID].waveBearing = this.bearing2compassRose(radar.waveHourlyData[tmst].WAVB);
-                this.radarData[radar.UUID].waveBearingValue = radar.waveHourlyData[tmst].WAVB;
-            } else
-              this.radarData[radar.UUID].waveHeight = undefined;
-            // Update wind data
-            if (radar.windHourlyData[tmst]){
-              this.radarData[radar.UUID].windBearingValue = radar.windHourlyData[tmst].WNDB;
-              this.radarData[radar.UUID].windBearing = this.bearing2compassRose(radar.windHourlyData[tmst].WNDB);
-            } else
-            this.radarData[radar.UUID].windBearing = undefined;
-          } else {
-            // If it exists, do not show data
-            if (this.radarData[radar.UUID] != undefined){
-              this.radarData[radar.UUID].waveHeight = undefined;
+              // Update wave data
+              if (radar.waveHourlyData[tmst]){
+                  this.radarData[radar.UUID].waveHeight = radar.waveHourlyData[tmst].MWHT.toFixed(2);
+                  this.radarData[radar.UUID].wavePeriod = radar.waveHourlyData[tmst].MWPD.toFixed(1);
+                  this.radarData[radar.UUID].waveBearing = this.bearing2compassRose(radar.waveHourlyData[tmst].WAVB);
+                  this.radarData[radar.UUID].waveBearingValue = radar.waveHourlyData[tmst].WAVB;
+              } else
+                this.radarData[radar.UUID].waveHeight = undefined;
+              // Update wind data
+              if (radar.windHourlyData[tmst]){
+                this.radarData[radar.UUID].windBearingValue = radar.windHourlyData[tmst].WNDB;
+                this.radarData[radar.UUID].windBearing = this.bearing2compassRose(radar.windHourlyData[tmst].WNDB);
+              } else
               this.radarData[radar.UUID].windBearing = undefined;
-              this.radarData[radar.UUID].hasData = false;
+            } else {
+              // If it exists, do not show data
+              if (this.radarData[radar.UUID] != undefined){
+                this.radarData[radar.UUID].waveHeight = undefined;
+                this.radarData[radar.UUID].windBearing = undefined;
+                this.radarData[radar.UUID].hasData = false;
+              }
             }
+          }
+
+          // File exists and it is loaded
+          if (radar.data[tmst] != undefined){
+            this.radarData[radar.UUID].fileStatus = 2;
+          } 
+          // File is loading
+          else if (window.DataManager.hourlyDataAvailability[tmst.substring(0,13) + 'Z']) {
+            if (window.DataManager.hourlyDataAvailability[tmst.substring(0,13) + 'Z'][radar.Site] == 3) {
+              this.radarData[radar.UUID].fileStatus = 3;
+              console.log("FILE STATUS LOADING")
+            }
+          }
+          // File does not exist
+          else {
+            this.radarData[radar.UUID].fileStatus = 0;
           }
           
         }
@@ -181,6 +246,20 @@ export default {
   border-radius: 17px;
 
   transition: all 1s;
+}
+
+.iconNoFile {
+  background: var(--red);
+}
+.iconLoading {
+  background: rgb(255, 255, 104);
+  animation: loadingColorAnimation 1s infinite;
+}
+
+@keyframes loadingColorAnimation {
+  0% {background: rgb(255, 255, 104); }
+  50% {background: rgb(138, 138, 0); }
+  100% {background: rgb(255, 255, 104);}
 }
 
 .hide {
