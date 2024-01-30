7f1bfd8f66459a82e4537192491420c2304f8688
Gerard Llorach <gerard.llorach@gmail.com>
2024-01-30 11:18:04 +0100

    changed button size for radars and external in-situ observations

--- Components/Map/OverlayWaveData.vue
+++ Components/Map/OverlayWaveData.vue
@@ -267,9 +267,9 @@ a {
 }
 
 .radarIcon {
-  width: 35px;
-  height: 35px;
-  margin-right: -17.5px; /* half of its width */
+  width: 1.6em;
+  height: 1.6em;
+  margin-right: -0.8em; /* half of its width */
 }
 .wavepanel {
   background: rgb(15 48 98 / 71%);/*var(--darkBlue);*/
