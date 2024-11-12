dddcc9e2625838b9c5880fc0be87b056f38d6cf1
Gerard Llorach <gerard.llorach@gmail.com>
2024-11-12 20:47:25 +0100

    corrected binding map events (and prettified)

--- Components/Map/ClimaDirectionCanvas.vue
+++ Components/Map/ClimaDirectionCanvas.vue
@@ -18,7 +18,7 @@ to fill the whole screen with equally spaced HTML elements, without causing over
 
 export default {
   name: 'climaDirectionCanvas',
-  created(){},
+  created() { },
   mounted() {
     // Canvas size
     this.createCanvas('dirCanvas');
@@ -37,7 +37,7 @@ export default {
       //this.map.updateSize();
     });
   },
-  data () {
+  data() {
     return {
 
     }
@@ -45,7 +45,7 @@ export default {
   methods: {
     // INTERNAL
     // Create canvas
-    createCanvas: function(canvasID){
+    createCanvas: function (canvasID) {
       let canvas = document.createElement("canvas");
       canvas.className = "position-absolute pe-none h-100 w-100";
       canvas.id = canvasID;
@@ -53,25 +53,31 @@ export default {
 
       // Test
       canvas.style["background-color"] = "rgba(1, 0, 0, 0.0)";
-      
+
       this.$refs.climaDirectionCanvas.appendChild(canvas);
 
       return canvas;
     },
 
     // New clima layer selected
-    setClimaLayer: function(infoWMTS){
+    setClimaLayer: function (infoWMTS) {
       // Hide if undefined (send undefined when closing widget)
-      if (infoWMTS == undefined){
+      if (infoWMTS == undefined) {
         this.$refs.climaDirectionCanvas.style.display = 'none';
+        // Remove callback events if defined
+        if (this.animEngine != undefined) {
+          // removeEventListener Map events for animation
+          this.$parent.map.un('moveend', this.animEngine.onMapMoveEnd);
+          this.$parent.map.un('movestart', this.animEngine.onMapMoveStart);
+        }
         return;
       } else {
         this.$refs.climaDirectionCanvas.style.display = 'revert';
       }
       // Check if it has animation parameter
-      if (infoWMTS.dataSet.animation){
+      if (infoWMTS.dataSet.animation) {
         // OL map
-        if (this.map == undefined){
+        if (this.map == undefined) {
           this.map = this.$parent.map;
         }
 
@@ -82,36 +88,44 @@ export default {
         infoWMTS.dataSet.animation.useArrows = true;
 
         // Create animation engine
-        if (this.animEngine == undefined){
+        if (this.animEngine == undefined) {
           this.animEngine = new AnimationEngine(this.canvas, this.map, infoWMTS, undefined);
           // Bind events
           // Map events for animation
           this.$parent.map.on('moveend', this.animEngine.onMapMoveEnd);
           this.$parent.map.on('movestart', this.animEngine.onMapMoveStart);
         } else {
+          if (this.animEngine.isStopped) {
+            // Map events for animation
+            this.$parent.map.on('moveend', this.animEngine.onMapMoveEnd);
+            this.$parent.map.on('movestart', this.animEngine.onMapMoveStart);
+          }
           this.animEngine.isStopped = false;
           this.animEngine.setWMTSSource(infoWMTS);
         }
         return
-      } 
+      }
       // Hide animation layer
       else {
-        if (this.animEngine != undefined){
+        if (this.animEngine != undefined) {
+          // removeEventListener Map events for animation
+          this.$parent.map.un('moveend', this.animEngine.onMapMoveEnd);
+          this.$parent.map.un('movestart', this.animEngine.onMapMoveStart);
           // Stop animation
           this.animEngine.isStopped = true;
           // Clear canvas
           this.animEngine.clearCanvas();
         }
-        
+
       }
     },
 
 
     // PUBLIC
     // Update canvas size and drawings
-    onMapMoveEnd: function(){
+    onMapMoveEnd: function () {
       // OL map
-      if (this.map == undefined){
+      if (this.map == undefined) {
         this.map = this.$parent.map;
       }
       // Resize animation canvas
@@ -131,7 +145,7 @@ export default {
 <style scoped>
 .climaDirectionCanvas {
   background: none;
-  position:absolute;
+  position: absolute;
   width: 100%;
   height: 100%;
   top: 0px;
