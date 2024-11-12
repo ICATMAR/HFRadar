6ca18c807ffcafb0eedcde272c2c4a30279b966a
Gerard Llorach <gerard.llorach@gmail.com>
2024-11-12 13:11:00 +0100

    216 images seems a lot to be loaded. probably will have to use map view extent and dynamically load the data

--- Assets/Scripts/AnimationEngine/SourceWMTS.js
+++ Assets/Scripts/AnimationEngine/SourceWMTS.js
@@ -1,5 +1,6 @@
 // Class that defines the source from WMTS images
-// FileManager is involved?
+// WMTSDataRetriever needs to be present
+// File Manager needs to be present
 
 class SourceWMTS {
   // Variables
@@ -9,7 +10,9 @@ class SourceWMTS {
   canvasLongLatStep = 0.02;
   colorrange;
 
-
+  medBBOX = [30, -17, 45, 30];
+  catseaBBOX = [-1, 36, 9, 44];
+  tileMatrixZoomLevel = 6;
 
   // Constructor
   constructor(infoWMTS) {
@@ -25,7 +28,7 @@ class SourceWMTS {
   }
 
   // Update/Change the WMTS Source
-  updateWMTSSource(WMTSURL, animation) {
+  updateWMTSSource(infoWMTS) {
     // In WMS, it was asking for an image covering the whole BBOX of the mediterranean.
     // In here we should check the tiles that need to be loaded according to:
     // - the med-BBOX -> only load once, but limited to this BBOX
@@ -38,7 +41,64 @@ class SourceWMTS {
 
     // We want to use FileManager, as openlayers map loads some data already.
     // This functions could be coded in WMTSDataRetriever. Question is, whole med-bbox or map-view-bbox (dynamic loading)? Also, which resolution? Maybe try different?
+
+    // Load all tiles that cover the mediterranean area
+    let rowLeft = window.WMTSDataRetriever.lon2tile4326(this.medBBOX[1], this.tileMatrixZoomLevel);
+    let rowRight = window.WMTSDataRetriever.lon2tile4326(this.medBBOX[3], this.tileMatrixZoomLevel);
+    let colTop = window.WMTSDataRetriever.lat2tile4326(this.medBBOX[2], this.tileMatrixZoomLevel);
+    let colBottom = window.WMTSDataRetriever.lat2tile4326(this.medBBOX[0], this.tileMatrixZoomLevel);
+
+    let tilesToLoad = [];
+    for (let row = rowLeft; row <= rowRight; row++) {
+      for (let col = colTop; col <= colBottom; col++) {
+        tilesToLoad.push([row, col]);
+      }
+    }
+
+    let urls = [];
+
+    // Get dataset according to timestamp
+    debugger;
+    let dataSet = infoWMTS.dataSet;
+    let animData = infoWMTS.dataSet.animation;
+
+    // Construct WMTS url
+    let templateURL = dataSet.template.replace('{Time}', infoWMTS.tmst);
+    // Projection
+    templateURL = templateURL.replace('{TileMatrixSet}', 'EPSG:4326'); //EPSG:4326, 3857
+    // Tile matrix
+    templateURL = templateURL.replace('{TileMatrix}', this.tileMatrixZoomLevel);
     
+    // Angle format
+    if (animData.format == 'value_angle') {
+      // Prepare url. Template uses the dataSet id in the layer info
+      templateURL = templateURL.replace('/' + dataSet.id + '&', '/' + animData.layerNames[1] + '&');  //url = WMTSDataRetriever.setWMSParameter(url, 'LAYERS', animData.layerNames[1]);
+      templateURL = window.WMTSDataRetriever.setWMTSParameter(templateURL, 'style', 'range:0/360,cmap:gray'); // url = WMTSDataRetriever.setWMSParameter(url, 'COLORSCALERANGE', String([-360,360]));
+      // Fill urls
+      for (let i = 0; i < tilesToLoad.length; i++){
+        urls.push(templateURL.replace('{TileCol}', tilesToLoad[i][0]).replace('{TileRow}', tilesToLoad[i][1]));
+      }
+    }
+    // East-North format
+    else if (animData.format == 'east_north') {
+      // Range (neg, pos)
+      let range = animData.range || dataSet.range;
+      templateURL = window.WMTSDataRetriever.setWMTSParameter(templateURL, 'style', 'range:' + range[0] + '/' + range[1] + ',cmap:gray');
+      // East-North layer
+      let urlEast = templateURL.replace('/' + dataSet.id, '/' + animData.layerNames[0]);
+      let urlNorth = templateURL.replace('/' + dataSet.id, '/' + animData.layerNames[1]);
+      // Fill urls
+      for (let i = 0; i < tilesToLoad.length; i++){
+        urls.push(urlEast.replace('{TileCol}', tilesToLoad[i][0]).replace('{TileRow}', tilesToLoad[i][1]));
+        urls.push(urlNorth.replace('{TileCol}', tilesToLoad[i][0]).replace('{TileRow}', tilesToLoad[i][1]));
+      }
+    }
+
+
+
+
+    debugger;
+
 
     // Keep track of images loaded
     this.isReady = false;
@@ -88,11 +148,11 @@ class SourceWMTS {
     canvas.width = this.longExtension / this.canvasLongLatStep;
     canvas.height = this.latExtension / this.canvasLongLatStep;
 
-    let ctx = canvas.getContext('2d', {willReadFrequently: false});
+    let ctx = canvas.getContext('2d', { willReadFrequently: false });
     //document.body.append(canvas);
 
     // WMTS data layers
-    if (animation.layerNames.length == 2){
+    if (animation.layerNames.length == 2) {
       for (let i = 0; i < 2; i++) {
         // LAYER east and north or intensity and angle
         WMTSURL = SourceWMTS.setWMTSParameter(WMTSURL, 'LAYERS', animation.layerNames[i]);
@@ -100,11 +160,11 @@ class SourceWMTS {
         if (animation.format == 'value_angle' && i == 1)
           WMTSURL = SourceWMTS.setWMTSParameter(WMTSURL, 'COLORSCALERANGE', '0,360');
         // For data stored in east-north, there range should have negative values too
-          else if (animation.format == 'east_north'){
-            this.colorrange[0] = -this.colorrange[1];
+        else if (animation.format == 'east_north') {
+          this.colorrange[0] = -this.colorrange[1];
           WMTSURL = SourceWMTS.setWMTSParameter(WMTSURL, 'COLORSCALERANGE', this.colorrange.toString());
-        } 
-        
+        }
+
         // Get WMTS image data
         console.log("Loading data source WMTS images...");
         let img = new Image();
@@ -136,7 +196,7 @@ class SourceWMTS {
         console.log(WMTSURL);
       }
     }
-    
+
   }
 
 
@@ -179,11 +239,11 @@ class SourceWMTS {
     }
 
     // If image is not loaded
-    if (this.isReady == false){
+    if (this.isReady == false) {
       value[0] = undefined; value[1] = undefined; // Reset value
       return value;
     }
-      
+
 
     // Get closest pixel to long lat (nearest-neighbour interpolation)
     let colPixelPos = Math.round((long - minLong) / this.canvasLongLatStep);
@@ -207,12 +267,12 @@ class SourceWMTS {
 
     // Rescale RGB value to real unit
     // Images store East and North intensity
-    if (this.animation.format == 'east_north'){
+    if (this.animation.format == 'east_north') {
       value[0] = redE * (this.colorrange[1] - this.colorrange[0]) + this.colorrange[0]; // lat
       value[1] = redN * (this.colorrange[1] - this.colorrange[0]) + this.colorrange[0]; // long
     }
     // Images store Intensity and Angle in degrees
-    else if (this.animation.format == 'value_angle'){
+    else if (this.animation.format == 'value_angle') {
       let intensity = redE * (this.colorrange[1] - this.colorrange[0]) + this.colorrange[0];
       let angle = redN * 360;
       value[1] = - 0.1 * Math.cos(angle * Math.PI / 180) * intensity;
